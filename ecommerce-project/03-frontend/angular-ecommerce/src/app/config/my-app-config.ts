1890407d96e41fbcbe6ce71d6b8d2ba4f355460e
Gabriel Costa (JAG) <Gabriel.Costa2@flex.com>
2022-02-11 18:07:02 -0200

    adicionado projeto ecommerce

---
+++ ecommerce-project/03-frontend/angular-ecommerce/src/app/config/my-app-config.ts
@@ -0,0 +1,10 @@
+export default {
+
+    oidc: {
+        clientId: '0oa3snhg6pfob02e95d7',
+        issuer: 'https://dev-88040159.okta.com/oauth2/default',
+        redirectUri: 'http://localhost:4200/login/callback',
+        scopes: ['openid', 'profile', 'email']
+    }
+
+}
